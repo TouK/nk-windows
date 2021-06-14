a2a94ab10d3ab9aa6f9439c54ff2dc724447e2e2
Julian Wielga <j@touk.pl>
2021-06-14 16:10:18 +0200

    cleanup

--- src/hello-world.spec.tsx
+++ src/hello-world.spec.tsx
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom";
 
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import HelloWorld from "./hello-world2";
+import HelloWorld from "./hello-world";
 
 test('displays a "Hello World" message', () => {
   render(<HelloWorld />);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export { default as HelloWorld } from "./hello-world";
-export { default as HelloWorld2 } from "./hello-world2";
