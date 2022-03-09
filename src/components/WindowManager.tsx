83f083b6b7d009c8ae5db232f618515116fc777f
Julian Wielga <j@touk.pl>
2022-03-09 01:08:31 +0100

    fix(theme): correct default theme

--- src/components/WindowManager.tsx
+++ src/components/WindowManager.tsx
@@ -6,7 +6,20 @@ import { WindowManagerContextProvider } from "../context";
 import { ContentGetter } from "./window/WindowContent";
 import { WindowsContainer } from "./WindowsContainer";
 
-const defaultTheme = { backgroundOpacity: 0.9, backdropFilter: "blur(16px)" };
+const defaultTheme = {
+  backgroundOpacity: 0.9,
+  backdropFilter: "blur(16px)",
+  colors: {
+    focusColor: "#0082c2",
+    primaryBackground: "#FFFFFF",
+    secondaryBackground: "#CCCCCC",
+    borderColor: "#000000",
+    mutedColor: "#999999",
+  },
+  spacing: {
+    baseUnit: 5,
+  },
+};
 
 interface WindowManagerProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   theme?: AppTheme;
