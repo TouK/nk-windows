2dd009056db2f5706c6240203ccb2eb17e7b3f87
Julian Wielga <j@touk.pl>
2022-12-21 14:34:00 +0100

    test: update cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
+
+const snapshotsSettings = {
+  pluginVisualRegressionMaxDiffThreshold: 0.00004,
+  pluginVisualRegressionUpdateImages: false,
+  pluginVisualRegressionForceDeviceScaleFactor: false,
+};
 
 export default defineConfig({
   env: {
-    'cypress-plugin-snapshots': {
-      imageConfig: {
-        threshold: 0.00004,
-      },
-      updateSnapshots: false,
-    },
+    ...snapshotsSettings,
   },
+  defaultCommandTimeout: 10000,
   e2e: {
+    experimentalRunAllSpecs: true,
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
+      return require("./cypress/plugins/index.ts")(on, config);
     },
-    baseUrl: 'http://localhost:6006',
-    excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
+    baseUrl: "http://localhost:6006",
+    excludeSpecPattern: ["**/__snapshots__/*", "**/__image_snapshots__/*"],
   },
-})
+});
