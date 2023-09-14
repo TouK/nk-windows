fbb3287c34cb32d548fccb90c584e0a7ef202442
Julian Wielga <j@touk.pl>
2023-09-14 15:08:45 +0200

    fix: events in header buttons

--- src/components/window/header/HeaderButtonClose.tsx
+++ src/components/window/header/HeaderButtonClose.tsx
@@ -8,7 +8,7 @@ export interface HeaderButtonCloseProps {
 
 export function HeaderButtonClose({ closeDialog }: HeaderButtonCloseProps): JSX.Element {
   return (
-    <HeaderButton name="close" onClick={() => closeDialog()}>
+    <HeaderButton name="close" onPointerDown={() => closeDialog()}>
       <CloseIcon />
     </HeaderButton>
   );
