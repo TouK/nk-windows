383420731f962bc79208802a0d45152d3d5b6e43
Julian Wielga <j@touk.pl>
2023-09-18 18:10:26 +0200

    chore(deps-dev): storybook update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,14 @@
 module.exports = {
-  core: {
-    builder: "webpack5",
-  },
   stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
   ],
+
   typescript: {
     check: false,
     checkOptions: {},
@@ -32,4 +31,13 @@ module.exports = {
 
     return config;
   },
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 };
