d4ad55d918ed73e039ae28edeb670b50860af691
Dawid Poliszak <dawid@readonlyrest.com>
2024-05-22 13:32:15 +0200

    feat: react 18 support

--- src/components/window/WindowFrame.tsx
+++ src/components/window/WindowFrame.tsx
@@ -113,6 +113,7 @@ export function WindowFrame(props: PropsWithChildren<WindowFrameProps>): JSX.Ele
     minHeight = 140,
   } = props;
   const ref = useRef<HTMLDivElement>();
+  const nodeRef = useRef<HTMLDivElement>();
   const viewport = useViewportSize();
   const [position, setPosition] = useState<Coords>();
   const [size, setSize] = useState<Size>(() => ({ height, width }));
@@ -213,9 +214,13 @@ export function WindowFrame(props: PropsWithChildren<WindowFrameProps>): JSX.Ele
   }, [getSnapSide, maximized, resizable]);
 
   const onDragStop = useCallback(
-    (e, position) => {
+    (e, { x, y }) => {
+      if (!dragging.current) {
+        return;
+      }
+
       dragging.current = false;
-      savePosition(position);
+      savePosition({ x, y });
       onSideSnap?.(side, true);
       onSnapCallback?.({ name: Side[side], code: side });
       setSide(Side.none);
@@ -283,7 +288,7 @@ export function WindowFrame(props: PropsWithChildren<WindowFrameProps>): JSX.Ele
   return (
     <>
       {/*fallback animation for lazy loaded content*/}
-      <CSSTransition in={contentAvailable} timeout={250} classNames={fadeInAnimation}>
+      <CSSTransition nodeRef={nodeRef} in={contentAvailable} timeout={250} classNames={fadeInAnimation}>
         <CSSTransition in={maximized} timeout={250} classNames={zoomAnimation} onEnter={onEnter} onExited={onExited}>
           <Rnd
             disableDragging={maximized || !moveable}
@@ -308,7 +313,7 @@ export function WindowFrame(props: PropsWithChildren<WindowFrameProps>): JSX.Ele
             data-testid="window-frame"
           >
             {/* trap keyboard focus within group (windows opened since last modal) */}
-            <FocusLock className={css({ flex: 1 })} group={focusGroup} disabled={!focusGroup} returnFocus autoFocus>
+            <FocusLock ref={nodeRef} className={css({ flex: 1 })} group={focusGroup} disabled={!focusGroup} returnFocus autoFocus>
               <div
                 ref={ref}
                 onFocus={focus}
