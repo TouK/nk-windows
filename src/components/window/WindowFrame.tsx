f9bef70cff59451f0f60e73dcabb93a7e2ec0a9a
Dawid Poliszak <dawid@readonlyrest.com>
2024-05-23 13:50:08 +0200

    NU-974 fix test

--- src/components/window/WindowFrame.tsx
+++ src/components/window/WindowFrame.tsx
@@ -113,6 +113,7 @@ export function WindowFrame(props: PropsWithChildren<WindowFrameProps>): JSX.Ele
     minHeight = 140,
   } = props;
   const ref = useRef<HTMLDivElement>();
+  const nodeRef = useRef<HTMLDivElement>();
   const viewport = useViewportSize();
   const [position, setPosition] = useState<Coords>();
   const [size, setSize] = useState<Size>(() => ({ height, width }));
@@ -283,7 +284,7 @@ export function WindowFrame(props: PropsWithChildren<WindowFrameProps>): JSX.Ele
   return (
     <>
       {/*fallback animation for lazy loaded content*/}
-      <CSSTransition in={contentAvailable} timeout={250} classNames={fadeInAnimation}>
+      <CSSTransition nodeRef={nodeRef} in={contentAvailable} timeout={250} classNames={fadeInAnimation}>
         <CSSTransition in={maximized} timeout={250} classNames={zoomAnimation} onEnter={onEnter} onExited={onExited}>
           <Rnd
             disableDragging={maximized || !moveable}
@@ -308,7 +309,7 @@ export function WindowFrame(props: PropsWithChildren<WindowFrameProps>): JSX.Ele
             data-testid="window-frame"
           >
             {/* trap keyboard focus within group (windows opened since last modal) */}
-            <FocusLock className={css({ flex: 1 })} group={focusGroup} disabled={!focusGroup} returnFocus autoFocus>
+            <FocusLock ref={nodeRef} className={css({ flex: 1 })} group={focusGroup} disabled={!focusGroup} returnFocus autoFocus>
               <div
                 ref={ref}
                 onFocus={focus}
