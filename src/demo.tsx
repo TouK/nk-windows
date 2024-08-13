aeb1e1f34fb4f28579093223c49c964e1e9819a4
Julian Wielga <j@touk.pl>
2024-08-14 01:16:44 +0200

    chore: docs deploy

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { name, version } from "../package.json";
 import { ContentGetter } from "./demo/contentGetter";
 import { DebugButtons } from "./demo/DebugButtons";
 import { DemoWindowKind } from "./demo/DemoWindowKind";
 import { WindowManagerProvider } from "./index";
+
+const Header: React.FC = () => {
+  if (window["Cypress"]) return null;
+
+  return (
+    <header style={{ fontFamily: "monospace" }}>
+      <h1>{name}</h1>
+      <h2>v{version}</h2>
+    </header>
+  );
+};
 
 const Demo: React.FC = () => {
   return (
@@ -14,6 +26,7 @@ const Demo: React.FC = () => {
         },
       }}
     >
+      <Header />
       <DebugButtons />
     </WindowManagerProvider>
   );
