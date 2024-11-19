10a67845358b270e70785ccaf4ea19f9185ea26f
Dawid Poliszak <dawid@readonlyrest.com>
2024-11-19 11:45:34 +0100

    chore: change animation library to react-transition-state

--- src/components/getFadeInAnimation.ts
+++ src/components/getFadeInAnimation.ts
@@ -1,23 +1,19 @@
 import { css } from "@emotion/css";
 
 export const getFadeInAnimation = (t = 0.25) => ({
-  enter: css({
-    opacity: 0,
-  }),
-  enterActive: css({
-    opacity: 1,
-    transition: `opacity ${t}s ease-in-out`,
-    pointerEvents: "none",
-  }),
-  exit: css({
-    opacity: 1,
-  }),
-  exitActive: css({
+  animationTime: t,
+  mounted: css({
     opacity: 0,
     transition: `opacity ${t}s ease-in-out`,
-    pointerEvents: "none",
+  }),
+  entered: css({
+    opacity: 1,
+  }),
+  exited: css({
+    opacity: 0,
   }),
 });
 
+export type FadeInAnimation = ReturnType<typeof getFadeInAnimation>;
 export const defaultFadeAnimation = getFadeInAnimation();
 export const fastFadeAnimation = getFadeInAnimation(0.15);
