e340ff3d76db337e68705aa0dc179e8421d193c8
Julian Wielga <j@touk.pl>
2025-10-23 13:11:04 +0200

    feat: expose DefaultContent ref

--- src/components/window/WindowContentGrid.tsx
+++ src/components/window/WindowContentGrid.tsx
@@ -1,12 +1,8 @@
-import { css, cx } from "@emotion/css";
-import React, { DetailedHTMLProps, HTMLAttributes } from "react";
+import styled from "@emotion/styled";
 
-export function WindowContentGrid({ className, ...props }: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>): JSX.Element {
-  const contentClass = css({
-    width: "100%",
-    height: "100%",
-    display: "grid",
-    gridTemplateRows: "auto 1fr auto",
-  });
-  return <div className={cx(contentClass, className)} {...props} />;
-}
+export const WindowContentGrid = styled("div")({
+  width: "100%",
+  height: "100%",
+  display: "grid",
+  gridTemplateRows: "auto 1fr auto",
+});
